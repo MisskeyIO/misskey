5451c6265a54933423fc515d104b833ac56ea244
riku6460 <17585784+riku6460@users.noreply.github.com>
2023-07-03 05:55:28 +0900

    cypress のタイムアウトを 10000 ms にする

--- cypress.config.ts
+++ cypress.config.ts
@@ -8,5 +8,6 @@ export default defineConfig({
       return require('./cypress/plugins/index.js')(on, config)
     },
     baseUrl: 'http://localhost:61812',
+    defaultCommandTimeout: 10000,
   },
 })

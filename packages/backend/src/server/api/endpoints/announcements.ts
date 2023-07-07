1d63c68c111fdddc4ae0d9ea5a9cde869120c704
CyberRex <hspwinx86@gmail.com>
2023-07-07 22:59:18 +0900

    一般向けAPIにはuserオブジェクトを提供しない

--- packages/backend/src/server/api/endpoints/announcements.ts
+++ packages/backend/src/server/api/endpoints/announcements.ts
@@ -2,7 +2,7 @@ import { Inject, Injectable } from '@nestjs/common';
 import { Endpoint } from '@/server/api/endpoint-base.js';
 import { QueryService } from '@/core/QueryService.js';
 import { DI } from '@/di-symbols.js';
-import type { AnnouncementReadsRepository, AnnouncementsRepository, UsersRepository } from '@/models/index.js';
+import type { AnnouncementReadsRepository, AnnouncementsRepository} from '@/models/index.js';
 
 export const meta = {
 	tags: ['meta'],
@@ -55,7 +55,7 @@ export const meta = {
 				user: {
 					type: 'object',
 					optional: false, nullable: true,
-				}
+				},
 			},
 		},
 	},
@@ -82,9 +82,6 @@ export default class extends Endpoint<typeof meta, typeof paramDef> {
 
 		@Inject(DI.announcementReadsRepository)
 		private announcementReadsRepository: AnnouncementReadsRepository,
-		
-		@Inject(DI.usersRepository)
-		private usersRepository: UsersRepository,
 
 		private queryService: QueryService,
 	) {
@@ -108,30 +105,17 @@ export default class extends Endpoint<typeof meta, typeof paramDef> {
 				const reads = (await this.announcementReadsRepository.findBy({
 					userId: me.id,
 				})).map(x => x.announcementId);
-				
+
 				for (const announcement of announcements) {
 					(announcement as any).isRead = reads.includes(announcement.id);
 				}
 			}
 
-			const resultbase = (ps.withUnreads ? announcements.filter((a: any) => !a.isRead) : announcements).map((a) => ({
+			return (ps.withUnreads ? announcements.filter((a: any) => !a.isRead) : announcements).map((a) => ({
 				...a,
 				createdAt: a.createdAt.toISOString(),
 				updatedAt: a.updatedAt?.toISOString() ?? null,
 			}));
-			for (let i = 0; i < resultbase.length; i++) {
-				const r = resultbase[i];
-				const userId = resultbase[i].userId;
-				if (userId) {
-					const user = await this.usersRepository.findOneBy({ id: userId });
-					if (user) {
-						r.user = user;
-						resultbase[i] = r;
-					}
-				}
-			}
-
-			return resultbase;
 		});
 	}
 }

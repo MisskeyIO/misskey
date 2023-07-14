304ce2466b243f0181ed422b8f0b083a644617be
okayurisotto <okayurisotto@proton.me>
2023-07-14 10:45:01 +0900

    Revert "refactor: 可読性のため一部で`Array.prototype.at`を使うように (#11274)"
    This reverts commit 2b6dbd4fcbec380d65b0c318932d9eeb3fcb3f7b.

--- packages/backend/src/queue/processors/CleanRemoteFilesProcessorService.ts
+++ packages/backend/src/queue/processors/CleanRemoteFilesProcessorService.ts
@@ -1,7 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { IsNull, MoreThan, Not } from 'typeorm';
 import { DI } from '@/di-symbols.js';
-import type { DriveFile, DriveFilesRepository } from '@/models/index.js';
+import type { DriveFilesRepository } from '@/models/index.js';
 import type { Config } from '@/config.js';
 import type Logger from '@/logger.js';
 import { DriveService } from '@/core/DriveService.js';
@@ -31,7 +31,7 @@ export class CleanRemoteFilesProcessorService {
 		this.logger.info('Deleting cached remote files...');
 
 		let deletedCount = 0;
-		let cursor: DriveFile['id'] | null = null;
+		let cursor: any = null;
 
 		while (true) {
 			const files = await this.driveFilesRepository.find({
@@ -51,7 +51,7 @@ export class CleanRemoteFilesProcessorService {
 				break;
 			}
 
-			cursor = files.at(-1)?.id ?? null;
+			cursor = files[files.length - 1].id;
 
 			await Promise.all(files.map(file => this.driveService.deleteFileSync(file, true)));
 

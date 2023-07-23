4253c6c3f891623cda4eb58a574fb501f50153f3
CyberRex <hspwinx86@gmail.com>
2023-07-23 18:56:55 +0900

    Merge migration scripts

--- packages/backend/migration/1688647797135-userannouncement.js
+++ packages/backend/migration/1688647797135-userannouncement.js
@@ -3,9 +3,11 @@ export class Userannouncement1688647797135 {
 
     async up(queryRunner) {
         await queryRunner.query(`ALTER TABLE "announcement" ADD COLUMN "userId" character varying(32)`);
+        await queryRunner.query(`ALTER TABLE "announcement" ADD COLUMN "closeDuration" integer`);
     }
 
     async down(queryRunner) {
         await queryRunner.query(`ALTER TABLE "announcement" DROP COLUMN "userId"`);
+        await queryRunner.query(`ALTER TABLE "announcement" DROP COLUMN "closeDuration"`);
     }
 }

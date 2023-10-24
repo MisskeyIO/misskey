78425ac64be877fd00eb604168813a3bdda8282b
まっちゃとーにゅ <17376330+u1-liquid@users.noreply.github.com>
2023-10-24 20:54:48 +0900

    サーバーが強制終了された場合fetchInstanceMetadataが永遠に解除されない問題を修正

--- packages/backend/src/core/FetchInstanceMetadataService.ts
+++ packages/backend/src/core/FetchInstanceMetadataService.ts
@@ -53,6 +53,7 @@ export class FetchInstanceMetadataService {
 	@bindThis
 	public async tryLock(host: string): Promise<boolean> {
 		const mutex = await this.redisClient.set(`fetchInstanceMetadata:mutex:${host}`, '1', 'GET');
+		await this.redisClient.expire(`fetchInstanceMetadata:mutex:${host}`, 60 * 5, 'NX');
 		return mutex !== '1';
 	}
 

41745bfe373ec00f5a7657723e8748ae301e5060
まっちゃとーにゅ <17376330+u1-liquid@users.noreply.github.com>
2023-11-07 20:19:22 +0900

    enhance(frontend): シェーダーコンパイルに失敗した場合詳細なエラー内容を出力できるように

--- packages/frontend/src/components/MkAnimBg.vue
+++ packages/frontend/src/components/MkAnimBg.vue
@@ -24,8 +24,16 @@ const props = withDefaults(defineProps<{
 function loadShader(gl, type, source) {
 	const shader = gl.createShader(type);
 
-	gl.shaderSource(shader, source);
-	gl.compileShader(shader);
+	try {
+		gl.shaderSource(shader, source);
+		gl.compileShader(shader);
+	} catch (e) {
+		alert(
+			`failed to compile shader: ${gl.getShaderInfoLog(shader)}`,
+		);
+		gl.deleteShader(shader);
+		return null;
+	}
 
 	if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
 		alert(

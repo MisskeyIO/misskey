bdd95219206ddf12f686347a7d71b194a4da76db
まっちゃとーにゅ <17376330+u1-liquid@users.noreply.github.com>
2023-11-18 23:51:34 +0900

    Update MkUpdated.vue

--- packages/frontend/src/components/MkUpdated.vue
+++ packages/frontend/src/components/MkUpdated.vue
@@ -27,7 +27,7 @@ const modal = shallowRef<InstanceType<typeof MkModal>>();
 
 const whatIsNew = () => {
 	modal.value.close();
-	window.open('https://misskeyhq.fanbox.cc/posts', '_blank');
+	window.open('https://go.misskey.io/changelog', '_blank');
 };
 
 onMounted(() => {

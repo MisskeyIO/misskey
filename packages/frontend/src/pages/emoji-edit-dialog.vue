621c82a0e9984909a10619edf848a05dec4568ff
Misaki <60120497+Misaki0331@users.noreply.github.com>
2024-04-28 07:38:07 +0900

    インデント修正

--- packages/frontend/src/pages/emoji-edit-dialog.vue
+++ packages/frontend/src/pages/emoji-edit-dialog.vue
@@ -167,7 +167,7 @@ async function changeImage(ev) {
 	file.value = await selectFile(ev.currentTarget ?? ev.target, null);
 	const candidate = file.value.name.replace(/\.(.+)$/, '');
 	if (candidate.match(/^[a-z0-9_]+$/)) {
-		if(!name.value) {
+		if (!name.value) {
 			name.value = candidate;
 		}
 	}

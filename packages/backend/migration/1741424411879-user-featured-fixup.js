d8e7648b1ac803b9acf81eb29204d71308b8e007
mattyatea <mattyacocacora0@gmail.com>
2025-07-13 04:42:49 +0900

    SQL文の最適化

--- packages/backend/migration/1741424411879-user-featured-fixup.js
+++ packages/backend/migration/1741424411879-user-featured-fixup.js
@@ -17,7 +17,7 @@ export class UserFeaturedFixup1741424411879 {
         $$ LANGUAGE sql IMMUTABLE;`);
 
         // "host" is NOT NULL is not needed but just in case add it to prevent overwriting irreplaceable data
-        await queryRunner.query(`UPDATE "user" SET "featured" = pg_temp.extract_ap_id("featured") WHERE "host" IS NOT NULL`);
+        await queryRunner.query(`UPDATE "user" SET "featured" = pg_temp.extract_ap_id("featured") WHERE "host" IS NOT NULL AND starts_with(featured, '{')`);
     }
 
     async down(queryRunner) {

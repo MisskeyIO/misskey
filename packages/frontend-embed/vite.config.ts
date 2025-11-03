f08b01edbe77be10b57ac49561142a3975180668
あわわわとーにゅ <17376330+u1-liquid@users.noreply.github.com>
2025-11-04 02:29:37 +0900

    t

--- packages/frontend-embed/vite.config.ts
+++ packages/frontend-embed/vite.config.ts
@@ -155,11 +155,6 @@ export function getConfig(): UserConfig {
 			emptyOutDir: false,
 			sourcemap: process.env.NODE_ENV === 'development',
 			reportCompressedSize: false,
-
-			// https://vitejs.dev/guide/dep-pre-bundling.html#monorepos-and-linked-dependencies
-			commonjsOptions: {
-				include: [/misskey-js/, /node_modules/],
-			},
 		},
 
 		worker: {

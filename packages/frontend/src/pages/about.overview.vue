9ae0be5cfcae64f41804bfc3bd2bf5e8c5659556
あわわわとーにゅ <17376330+u1-liquid@users.noreply.github.com>
2025-11-22 03:06:42 +0900

    chore: fix merge error

--- packages/frontend/src/pages/about.overview.vue
+++ packages/frontend/src/pages/about.overview.vue
@@ -7,7 +7,9 @@ SPDX-License-Identifier: AGPL-3.0-only
 <div class="_gaps_m">
 	<div :class="$style.banner" :style="{ backgroundImage: `url(${ instance.bannerUrl })` }">
 		<div style="overflow: clip;">
-			<img :src="instance.iconUrl ?? instance.faviconUrl ?? '/favicon.ico'" alt="" :class="$style.bannerIcon"/>
+			<img v-if="kawaiiMode" src="/client-assets/kawaii/misskey.png" alt="" :class="$style.bannerIconAlt"/>
+			<img v-else :src="instance.iconUrl ?? instance.faviconUrl ?? '/favicon.ico'" alt="" :class="$style.bannerIcon"/>
+			<Mfm v-if="kawaiiMode" text="Logo by @sawaratsuki@misskey.io" :class="$style.iconCredit"/>
 			<div :class="$style.bannerName">
 				<b>{{ instance.name ?? host }}</b>
 			</div>
@@ -150,6 +152,12 @@ const initStats = () => misskeyApi('stats', {});
 	margin: 16px auto 0 auto;
 	height: 64px;
 	border-radius: 8px;
+}
+
+.bannerIconAlt {
+	display: block;
+	margin: 16px auto 0 auto;
+	width: 85%;
 }
 
 .bannerName {

d3aa66d25302e2d2e9ca3b8f65f35c7992209c12
あわわわとーにゅ <17376330+u1-liquid@users.noreply.github.com>
2025-12-11 00:14:04 +0900

    spec: sentryにsourcemapをアップロードするように

--- packages/frontend-embed/vite.config.ts
+++ packages/frontend-embed/vite.config.ts
@@ -153,7 +153,7 @@ export function getConfig(): UserConfig {
 			outDir: __dirname + '/../../built/_frontend_embed_vite_',
 			assetsDir: '.',
 			emptyOutDir: false,
-			sourcemap: process.env.NODE_ENV === 'development',
+			sourcemap: true,
 			reportCompressedSize: false,
 		},
 

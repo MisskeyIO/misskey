c568e33875f52cc81db39f0a61a4513f04d12981
mattyatea <mattyacocacora0@gmail.com>
2026-03-11 19:53:37 +0900

    fix(frontend-shared): guard matchMedia in test env

--- packages/frontend-shared/js/config.ts
+++ packages/frontend-shared/js/config.ts
@@ -20,4 +20,6 @@ export const instanceName = (siteName === 'Misskey' || siteName == null) ? host 
 export const ui = localStorage.getItem('ui');
 export const debug = localStorage.getItem('debug') === 'true';
 export const isSafeMode = localStorage.getItem('isSafeMode') === 'true';
-export const prefersReducedMotion = window.matchMedia('(prefers-reduced-motion)').matches;
+export const prefersReducedMotion = typeof window.matchMedia === 'function'
+	? window.matchMedia('(prefers-reduced-motion)').matches
+	: false;

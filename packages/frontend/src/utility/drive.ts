8a8bcef665b67944fcf14ba5ee7e6307d97ae768
SanMurakami <murakami@misskey.io>
2026-09-02 18:49:13 +0900

    アップロードのタイムアウト理由を表示

--- packages/frontend/src/utility/drive.ts
+++ packages/frontend/src/utility/drive.ts
@@ -81,6 +81,12 @@ export function uploadFile(file: File | Blob, options: {
 						title: i18n.ts.failedToUpload,
 						text: i18n.ts.cannotUploadBecauseExceedsFileSizeLimit,
 					});
+				} else if (xhr.status === 524) {
+					os.alert({
+						type: 'error',
+						title: i18n.ts.failedToUpload,
+						text: i18n.ts.cannotUploadBecauseTimeout,
+					});
 				} else if (ev.target?.response) {
 					const res = JSON.parse(ev.target.response);
 					if (res.error?.id === 'bec5bd69-fba3-43c9-b4fb-2894b66ad5d2') {
